33e88fc4392072a21f0df2be9c483a203bd0a9b6
Dawid Sitnik <sitnik.dawid1@gmail.com>
2018-10-25 15:10:45 +0900

    Add files via upload

---
+++ BeImmortal Strona/js/sitnik.js
@@ -0,0 +1,18 @@
+$(window).scroll(function(){
+	$('nav#mainNav.navbar.navbar-expand-lg.navbar-light.fixed-top').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+$('#modal1').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+});
+
+$('#modal6').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal6 iframe').attr("src", $("#modal6 iframe").attr("src"));
+});
+
+$('#modal4').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal4 iframe').attr("src", $("#modal4 iframe").attr("src"));
+});
